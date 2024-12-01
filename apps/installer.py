8f1587f4b17638e16704b79d1224078cac8b4534
Michael <114355072+aRandomTurnip@users.noreply.github.com>
2024-12-01 11:44:04 -0800

    Update installer.py

--- apps/installer.py
+++ apps/installer.py
@@ -1,1 +1,7 @@
-
+item = str(input("What would you like to install?\n0. App\n1. OS\n"))
+if item == "0":
+ apps.append(requests.get("https://arandomturnip.github.io/pyos/apps/") + str(input("Package Name:\n")
+ appnames.append(input("App Display Name:\n")
+elif itme == "1":
+ oscodes.append(requests.get("https://arandomturnip.github.io/pyos/os/") + str(input("Package Name:\n")
+ osnames.append(input("OS Display Name:\n")
